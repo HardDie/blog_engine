4be52de018d2942bb1eb716a3de308f55878018d
HardDie <oleg1995sysoev@yandex.ru>
2024-05-12 01:31:40 +0300

    Access-Control-Allow-Credentials

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -16,6 +16,7 @@ func SetupCors(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PATCH,UPDATE,DELETE,OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, Authorization")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	if r.Method == http.MethodOptions {
 		return
 	}
